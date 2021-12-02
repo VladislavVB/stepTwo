dba91f763571796975fcbf3803b83d3a99913cb7
Vlad Vlad <burdukovskij.vlad@mail.ru>
2021-12-03 00:01:45 +0300

    form

--- app/js/home.js
+++ app/js/home.js
@@ -1,11 +1,38 @@
-const mainScreenButton = document.querySelector('.main-screen-button');
-const closeSafari = document.querySelector('.closeSafari');
-const safari = document.querySelector('.safari');
+const sendBtn = document.querySelector('.form-bottom button');
 
-mainScreenButton.onclick = () => {
-  safari.classList.add('active');
-}
 
-closeSafari.onclick = () => {
-  safari.classList.remove('active');
+
+
+const toSubmit = (e) => {
+  e.preventDefault();
+
+
+  const formItemInput = document.querySelector('.form-item-input');
+  const radioBtns = document.querySelectorAll('.type-radio');
+  const howLike = document.querySelectorAll('.how-like');
+  const howDontLike = document.querySelectorAll('.how-dontlike');
+  const formData = [];
+  const howLikeData = []
+  const howDontLikeData = [];
+
+
+  formData.push(formItemInput.value)
+  radioBtns.forEach(elem => {
+    if(elem.checked) {
+      formData.push(elem.value)
+    }
+  });
+  howLike.forEach(elem => {
+    if(elem.checked) {
+      howLikeData.push(elem.value)
+      formData.push(howLikeData)
+    }
+  })
+  howDontLike.forEach(elem => {
+    if(elem.checked) {
+      howDontLikeData.push(elem.value)
+      formData.push(howDontLikeData)
+    }
+  })
+  console.log(formData);
 }
